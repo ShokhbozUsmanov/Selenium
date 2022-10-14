c68614ebbdd93042a0ed6eb918554eefe17b3aa5
ShokhbozUsmanov <tashcity@mail.ru>
2022-10-15 00:35:17 +0500

    upd

--- src/test/java/day10/JavaFaker_Practice.java
+++ src/test/java/day10/JavaFaker_Practice.java
@@ -7,14 +7,15 @@ public class JavaFaker_Practice {
     @Test
     public void java_faker_test(){
         Faker faker = new Faker();
-
         System.out.println("faker.name().firstName() = " + faker.name().firstName());
         System.out.println("faker.name().lastName() = " + faker.name().lastName());
         System.out.println("faker.name().fullName() = " + faker.name().fullName());
         System.out.println("faker.address().city() = " + faker.address().city());
         System.out.println("faker.numerify(\"###########\") = " + faker.numerify("###########"));
         System.out.println("faker.bothify(\"##??##???\") = " + faker.bothify("##??##???"));
-
+        System.out.println("faker.finance().creditCard() = " + faker.finance().creditCard());
+        System.out.println("faker.finance().creditCard() = " + faker.finance().creditCard());
+        System.out.println("faker.chuckNorris().fact() = " + faker.chuckNorris().fact());
 
     }
 }
---
+++ src/test/java/day10/TestingSingleton.java
@@ -0,0 +1,31 @@
+package day10;
+
+import org.testng.annotations.Test;
+
+public class TestingSingleton {
+
+    @Test
+    public void singleton_understand_test(){
+        String str1 = Singleton.getWord();
+        System.out.println(str1);
+    }
+
+    @Test
+    public void singleton_understand_test2(){
+        String str4 = Singleton.getWord();
+        System.out.println("str1 = "+str4);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
